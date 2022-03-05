05c9dd6b54d86909e9d79617793269575d998e8b
lArkl <g.ferro.palomino@gmail.com>
2022-03-05 18:34:26 -0500

    added formatting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react"
+import React from "react";
 import UserComments from "./components/UserComments";
 
 function App() {
   return (
-  <main>
-    <UserComments />
-  </main>
-  )
+    <main>
+      <UserComments />
+    </main>
+  );
 }
 
 export default App;
