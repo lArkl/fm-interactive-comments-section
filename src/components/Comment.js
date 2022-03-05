6464abf50286f7ed96838a3219d697d055ba1a80
lArkl <g.ferro.palomino@gmail.com>
2022-03-05 18:39:10 -0500

    split comment sections into components

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,50 +1,42 @@
-import React, { useState } from 'react';
-import PlusImg from "../../images/icon-plus.svg";
-import MinusImg from "../../images/icon-minus.svg";
+import React, { useState } from "react";
 import ReplyImg from "../../images/icon-reply.svg";
 import images from "../../images/avatars/*.webp";
+import Scoring from "./Scoring";
+import Header from "./Header";
+import Actions from "./Actions";
 
-
-function Comment (props) {
-  const { content, createdAt, user, replies } = props.comment;
+function Comment({ comment, currentUsername }) {
+  const { content, createdAt, user, score, replies } = comment;
   const imgName = user.image.webp.match(/[-a-z]+(?=.webp$)/);
-  const [score, updateScore] = useState(props.score);
   return (
     <>
-    <div>
       <article className="comment">
-        <div className="comment-header">
-          <div className="comment-header__image">
-            <img src={images[imgName]} alt="user" />
-          </div>
-          <div className="comment-header__user">{user.username}</div>
-          <div className="comment-header__date">{createdAt}</div>
-        </div>
+        <Header
+          username={user.username}
+          imgName={imgName}
+          createdAt={createdAt}
+        />
         <div className="comment-content">
-          <p>
-            {content}
-          </p>
+          <p>{content}</p>
         </div>
-        <div className="comment-scoring">
-          <div><img src={PlusImg} alt="plus"/></div>
-          <div>{score}</div>
-          <div><img src={MinusImg} alt="minus"/></div>
-        </div>
-        <div className="comment-actions">
-          <div className="comment-actions__reply">
-            <img src={ReplyImg} alt="reply" />
-            Reply
-          </div>
+        <div className="options">
+          <Scoring score={score} />
+          <Actions isUser={user.username === currentUsername} />
         </div>
       </article>
-    </div>
-    <div className='replies'>
-      {replies && replies.map(reply => (
-        <Comment comment={reply} key={reply.id}/>
-      ))}
-    </div>
+      {replies && (
+        <div className="replies">
+          {replies.map((reply) => (
+            <Comment
+              comment={reply}
+              key={reply.id}
+              currentUsername={currentUsername}
+            />
+          ))}
+        </div>
+      )}
     </>
-  )
+  );
 }
 
 export default Comment;
