16741944ee57fd92f15a940d71cbfaaa777ca3f5
lArkl <g.ferro.palomino@gmail.com>
2022-03-20 20:08:49 -0500

    updated comment content value and style

--- src/components/CommentContent/CommentContent.js
+++ src/components/CommentContent/CommentContent.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./styles.css";
 
-function CommentContent({ isEditing, replyingTo, contentHook }, ref) {
+function CommentContent({ isEditing, replyingTo, contentHook }) {
   const [content, updateContent] = contentHook;
+  const defaultValue =
+    (replyingTo ? `@${replyingTo} ` : "") + `${replaceReply(content)}`;
   return (
     <div className="comment-content">
       {isEditing ? (
         <textarea
-          onChange={(ev) => updateContent(ev.target.value)}
-          defaultValue={`@${replyingTo} ${replaceReply(content)}`}
+          onChange={(ev) => {
+            updateContent(ev.target.value);
+          }}
+          defaultValue={defaultValue}
         ></textarea>
       ) : (
         <p>
